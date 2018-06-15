29348fb2404ef63684d0ad670082f6bb1b2af773
RichyWu <39182257+RichyWu@users.noreply.github.com>
2018-06-15 17:48:27 +0930

    加入一些icon

--- pages/result/result.js
+++ pages/result/result.js
@@ -1,14 +1,45 @@
 
+var util = require('../../utils/util.js');
+
 Page({
   data: {
-    logs: []
+    rate:0
   },
   onLoad: function () {
-
+    this.getCurrencyRate();
   },
 
   testInput() {
     console.log('input box has been clicked')
-  }
+  },
+
+  getCurrencyRate() {
+    var self = this;
+    var now = new Date();
+    var nowDate = util.formatDate(now, '-');
+
+    wx.request({
+      url: 'https://api.getweapp.com/thirdparty/95516/web.exchange.rate?date=' + nowDate + '&baseCurrency=CNY' + '&transactionCurrency=AUD',
+      header: {
+        "contentType": "application/json",
+        "dataType": "json"
+      },
+      success: function (res) {
+        var realRateValue = parseFloat(res.data.params.result);
+        if (realRateValue) {
+          self.setData({
+            rate: res.data.params.result
+          });
+          console.log('get rate successfully, it is 1:' + self.data.rate);
+        }
+        else {
+          console.log('request sent successfully but not get data')
+        }
+      },
+      fail: function (res) {
+        console.log('request failure')
+      }
+    });
+  },
 
 })
