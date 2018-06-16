bb8e4f2a2d7ecb73fc21b15026e6c5290304cfea
Richard Wu <rwu@cohda.wireless>
2018-06-16 18:59:06 +0930

    结果页把微留学花费计算完成了

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ App({
    
   },
   globalData: {
-    schoolLevelIdx: 0,
-    schoolFee: [278, 369, 414],
+    schoolLevelIdx: 0, // 0 - PS; 1 - junior; 2 senior
     learningWeeks: 0,
+    learnLevel:'',
     startDate: '',
     endDate:'',
     termStart: 0,
--- pages/result/result.js
+++ pages/result/result.js
@@ -1,12 +1,40 @@
 
 var util = require('../../utils/util.js');
+var app = getApp();
+const schoolFee = [278, 369, 414];//1st for PS; 2nd for junior; 3rd for senior
 
 Page({
   data: {
-    rate:0
+    rate:0, //人民币兑澳元汇率
+    schoolLevelIdx: 0, // 0 - PS; 1 - junior; 2 senior
+    learningWeeks: 0,
+    learnLevel: '',
+    startDate: '',
+    endDate: '',
+    termStart: 0,
+    termEnd: 0,
+    enrolFee: 284,
+    tuitionFee: 0,
+    serviceFee: 1000,
   },
   onLoad: function () {
     this.getCurrencyRate();
+    //计算学费
+    var learnWeeks = app.globalData.learningWeeks;
+    var schoolLevel = app.globalData.schoolLevelIdx;
+    var tuitionPerWeek = schoolFee[schoolLevel];
+    var tuition = learnWeeks * tuitionPerWeek;
+
+    this.setData({
+      schoolLevelIdx: schoolLevel,
+      learningWeeks: learnWeeks,
+      learnLevel: app.globalData.learnLevel,
+      startDate: app.globalData.startDate,
+      endDate: app.globalData.endDate,
+      termStart: app.globalData.termStart,
+      termEnd: app.globalData.termEnd,
+      tuitionFee: tuition,
+    })
   },
 
   testInput() {
@@ -15,7 +43,10 @@ Page({
 
   getCurrencyRate() {
     var self = this;
-    var now = new Date();
+    self.setData({
+      rate: 5,
+    });
+    /*var now = new Date();
     var nowDate = util.formatDate(now, '-');
 
     wx.request({
@@ -39,7 +70,7 @@ Page({
       fail: function (res) {
         console.log('request failure')
       }
-    });
+    });*/
   },
 
 })
